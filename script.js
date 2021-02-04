5d41af42d9e201722e6466d87e0cfeebc8592c9f
vanesa gonzalez <gvane1919@gmail.com>
2021-02-03 23:29:43 -0500

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+$(."list-group-item list-group-item-action1").on("click").append(){
+    {"coord":{"lon":2.3488,"lat":48.8534},"weather":[{"id":801,"main":"Clouds","description":"few clouds","icon":"02n"}],"base":"stations","main":{"temp":279.84,"feels_like":276.85,"temp_min":278.71,"temp_max":280.37,"pressure":1010,"humidity":87},"visibility":10000,"wind":{"speed":2.57,"deg":240},"clouds":{"all":20},"dt":1612393382,"sys":{"type":1,"id":6550,"country":"FR","sunrise":1612422988,"sunset":1612457555},"timezone":3600,"id":2988507,"name":"Paris","cod":200}
+}
+$(."list-group-item list-group-item-action2").on("click").append(){
+
+
+}
+$(."list-group-item list-group-item-action3").on("click").append(){
+    
+}
